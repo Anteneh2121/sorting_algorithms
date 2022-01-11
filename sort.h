3456fa171bb97b28f4e2e08b8c9a82c9dc9086f4
Anteneh Bizuneh <antukassaw1@gmail.com>
2022-01-11 12:32:15 -0800

    quick_sort_hoare

--- sort.h
+++ sort.h
@@ -40,5 +40,7 @@ void radix_sort(int *array, size_t size);
 void counting_sort_r(int *array, size_t size, int pos, int *out, int *ca);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
+int partition_hoare(int *a, int l, int h);
+
 
 #endif /* SORT_H */
